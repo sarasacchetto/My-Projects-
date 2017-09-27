8a740101851b81f577fdcf28d51b5fcf463cd829
sarasacchetto <32307062+sarasacchetto@users.noreply.github.com>
2017-09-27 12:49:16 +0200

    lesson 01 ex 02

--- sketch.js
+++ sketch.js
@@ -3,32 +3,28 @@ function setup() {
 }
 
 function draw() {
-  background(200,200,200);
-  strokeWeight(6);
-  //noStroke();
-  //stroke(0,255,255,0);
   
-  //colorMode(RGB);
-  stroke(255,255,255,255);
-  line(200,200,400,400); //if we want to hide sometigh, write the slash before 
+  background(200,200,200); //if we keep the opacity 50, the circle moves with a "tail"
+                // if we hide the background, the circles follows each other
   
-  stroke(0,255,0);
-  strokeWeight(10);
-  fill(0,100,0);
-  rect(50,50,300,150);
-  
-  stroke(255,0,0);
-  strokeWeight(1);
-  noFill();
-  ellipse(350,50,10,10);
-  
-  noStroke();
-  fill(0,200,200);
-  ellipse(50,50,10,20);
-  
-  strokeWeight(2);
- //colorMode(HSB);
-  stroke(255,0,0);
-  line(400,400,800,800); 
-  
+  fill(255,0,0); //red
+ rect(50,50,200,350);
+ 
+  fill(0,255,0); //green
+ rect(150,150,500,500);
+ 
+  fill(0,0,255);  //blue
+ rect(100,100,400,400);
+ 
+  fill(200,200,200);  //grey
+ rect(40,40,100,100);
+ 
+ 
+ ellipse(width/2,height*0.5, 150, 150);
+ ellipse(   (width/4)*3,height*0.5, 150, 150);
+ 
+ fill(0,0,255);
+ //ellipse(mouseX,mouseY,100,100);
+line(pmouseX,pmouseY,mouseX,mouseY);  //col background attivo si ha una linea  finita, col background disattivato si disegna veramente.
+ 
 }
